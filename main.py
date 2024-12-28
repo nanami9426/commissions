8bb9579170594e8370206288f10ca6fb1730b18c
zws <1156592100@qq.com>
2024-12-28 18:59:39 +0800

    修复跨域问题

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 from process import process_router
 from rec import rec_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(process_router)
 app.include_router(rec_router)
